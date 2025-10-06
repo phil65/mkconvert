4e813fc4e86d2ab9977bb29b136aab258a66d68c
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 19:28:03 +0200

    Update to copier template v2.0.5
    - Improved docstring formatting with proper line length handling
    - Better punctuation normalization (exactly one trailing period)
    - Removed __description__ variable for cleaner module namespace
    - Updated pre-commit hooks to latest versions

--- src/mkconvert/__init__.py
+++ src/mkconvert/__init__.py
@@ -1,4 +1,7 @@
-"""mkconvert: Markdown-HTML converters"""
+"""mkconvert: main package.
+
+Markdown-HTML converters.
+"""
 
 from __future__ import annotations
 
@@ -6,7 +9,7 @@ from importlib.metadata import version
 
 __version__ = version("mkconvert")
 __title__ = "mkconvert"
-__description__ = "Markdown-HTML converters"
+
 __author__ = "Philipp Temminghoff"
 __author_email__ = "philipptemminghoff@googlemail.com"
 __copyright__ = "Copyright (c) 2024 Philipp Temminghoff"
